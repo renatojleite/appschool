da91745c11ea84db6ac737a86bf027df22694479
Renato Nobrega <renatojleite@gmail.com>
2019-08-27 20:38:59 -0300

    Created namespace-ok

--- app/controllers/user/offers_controller.rb
+++ app/controllers/user/offers_controller.rb
@@ -4,4 +4,42 @@ class User::OffersController < ApplicationController
     @offers = current_user.offers
   end
 
+
+  def new
+    @offer = Offer.new
+  end
+
+def create
+    @offer = Offer.new(offer_params)
+    @offer.user = current_user
+    if @offer.save
+      redirect_to user_offers_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @offer = Offer.find(params[:id])
+  end
+
+  def update
+    @offer = Offer.find(params[:id])
+    if @offer.update(offer_params)
+    redirect_to offer_path(@offer)
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @offer = Offer.find(params[:id])
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:name, :description, :price, :category_id)
+  end
+
 end
