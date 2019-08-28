113da70688c61a1107b904a76ca545194d7bb8a2
Renato Nobrega <renatojleite@gmail.com>
2019-08-28 12:56:18 -0300

    buy-ok

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,4 +1,16 @@
 class DealsController < ApplicationController
+
   def create
+    @offer = Offer.find(params[:offer_id])
+    @deal = Deal.new
+    @deal.user = current_user
+    @deal.offer = @offer
+    if @deal.save
+      redirect_to offer_path(@offer)
+    else
+      render 'offers/show'
+    end
   end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'deals/create'
   devise_for :users
   root to: 'offers#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :offers, except: [ :edit, :update ]
+  resources :offers, except: [ :edit, :update ] do
+    resources :deals, only: :create
+  end
 
   namespace :user do
     resources :offers
