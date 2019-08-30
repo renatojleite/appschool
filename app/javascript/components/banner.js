b89330e46e74899a47f7893a98319e5318505ebc
Tomas Medina <tomymg11@hotmail.com>
2019-08-30 16:04:55 -0300

    solved

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,18 +1,13 @@
 import Typed from 'typed.js';
 
 const initTyped = () => {
-  new Typed('#typed', {
-    strings: ["Faça parte desta rede !!!"],
-    typeSpeed: 50,
-    loop: true
-  });
-
-  // new Typed('#typed', {
-  //   strings: ["The best way to match your deals with the planet !!!"],
-  //   typeSpeed: 50,
-  //   loop: true
-  // });
-
+  if(document.getElementById('typed')){
+    new Typed('#typed', {
+      strings: ["Faça parte desta rede !!!"],
+      typeSpeed: 50,
+      loop: true
+    });
+  }
 }
 
 
